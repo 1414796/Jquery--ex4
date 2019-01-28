4a6e5cad00e6e8cb711950266dd702e94b6c97cc
Tuong Vy <14147962hcmut.edu.vn>
2019-01-28 16:41:46 +0700

    initial

---
+++ js/script.js
@@ -0,0 +1,16 @@
+var move1 = $(".moving__1");
+var move2 = $(".moving__2");
+var move3 = $(".moving__3");
+
+setInterval(function () {
+    $(document).mousemove(function(event){
+        // Get pos of mouse
+        var posX = event.clientX;
+        // Animation
+        TweenLite.to(move1, 0, {x: (posX * 0.1 - 65), ease: Power1.easeOut }); // minus the start pos of layer2
+        TweenLite.to(move2, 0, {x: (posX * 0.2 - 170), ease: Power1.easeOut }); // minus the start pos of layer3
+        TweenLite.to(move3, 0, {x: (posX * 0.3 - 280), ease: Power1.easeOut });// minus the start pos of layer4
+
+
+    });
+});
